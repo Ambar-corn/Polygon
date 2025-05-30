0166d541e016f6009361dfed7572b4606f630310
Ambar-corn <efimsysoev2023@mail.ru>
2025-05-30 19:49:44 +0300

    _

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
-const isMobile = {
-    Android: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function() {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function() {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function() {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function() {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function() {
-        return (
-            isMobile.Android() ||
-            isMobile.BlackBerry() ||
-            isMobile.iOS() ||
-            isMobile.Opera() ||
-            isMobile.Windows()
-        );
-    }
+const mass = {
+  elec: [{ price: 42, name: "antonio" }],
+  text: [{ br: 123, jfs: "sdfsf" }],
 };
-
-if (isMobile.any()) {
-    document.body.classList.add('_touch');
-} else {
-    document.body.classList.add('_pc');
-}
+let masscop = mass;
+console.log(mass);
+masscop = {
+  elec: [{ price: 2, name: "antonio" }],
+  text: [{ br: 2, jfs: "sdfsf" }],
+};
+console.log(mass);
